99eacaa0eefe50c184744d17be6fcea013a50de3
Thomas Lavend'Homme <lavendthomas@outlook.be>
2021-12-03 13:45:22 +0100

    Use a single Pool connection to the PostgreSQL DB

--- src/db/phoeg.ts
+++ src/db/phoeg.ts
@@ -3,38 +3,50 @@ import {POSTGRESQL_LOGIN} from "../.env";
 import {createClient} from "redis";
 import {sha1} from "sha.js";
 
-const pool = new Pool({
-    user: POSTGRESQL_LOGIN.user,
-    host: POSTGRESQL_LOGIN.host,
-    database: POSTGRESQL_LOGIN.database,
-    password: POSTGRESQL_LOGIN.password,
-    port: POSTGRESQL_LOGIN.port,
-})
+class PooledDB {
+
+    private static instance: PooledDB
+
+    private static _pool: Pool
+
+    private constructor() {
+        PooledDB._pool = new Pool({
+            user: POSTGRESQL_LOGIN.user,
+            host: POSTGRESQL_LOGIN.host,
+            database: POSTGRESQL_LOGIN.database,
+            password: POSTGRESQL_LOGIN.password,
+            port: POSTGRESQL_LOGIN.port,
+        })
+    }
+
+    public static async getInstance() {
+        if (!PooledDB.instance) {
+            PooledDB.instance = new PooledDB();
+        }
+        await PooledDB._pool.connect()
+        return PooledDB.instance;
+    }
+
+    public async query(text: string, params: any[], callback: (err: Error, result: QueryResult) => any) {
+        return PooledDB._pool.query(text, params, callback)
+    }
+}
+
+
+class RedisCient {
+
+}
+
 
 export default {
     query: async (text: string, params: any[], callback: (err: Error, result: QueryResult) => any) => {
+        const psql_pool = await PooledDB.getInstance()
 
-        const client = new Client({
-            user: POSTGRESQL_LOGIN.user,
-            host: POSTGRESQL_LOGIN.host,
-            database: POSTGRESQL_LOGIN.database,
-            password: POSTGRESQL_LOGIN.password,
-            port: POSTGRESQL_LOGIN.port,
-        })
-
-        await client.connect()
-        return client.query(text, params, callback)
+        return psql_pool.query(text, params, callback)
     },
 
     cached_query: async (text: string, params: any[], callback: (err: Error, result: QueryResult) => any) => {
-        const psql_client = new Client({
-            user: POSTGRESQL_LOGIN.user,
-            host: POSTGRESQL_LOGIN.host,
-            database: POSTGRESQL_LOGIN.database,
-            password: POSTGRESQL_LOGIN.password,
-            port: POSTGRESQL_LOGIN.port,
-        })
-        await psql_client.connect()
+        const psql_pool = await PooledDB.getInstance()
 
         const redis_client = createClient({
             url: 'redis://:@localhost:6379' // No username nor password     is needed
@@ -49,7 +61,7 @@ export default {
             // @ts-ignore
             callback(null, query_result)
         } else {
-            return psql_client.query(text, params, (err: Error, result: QueryResult) => {
+            return psql_pool.query(text, params, (err: Error, result: QueryResult) => {
                 console.debug("Querying " + redis_key + " and adding it to the cache.")
                 redis_client.set(redis_key, JSON.stringify(result))
                 callback(err, result)
