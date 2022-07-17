c4d4e236d481c89f748ac3fc6256280637c57cdc
Thomas Lavend'Homme <lavendthomas@outlook.be>
2022-07-17 14:28:52 +0200

    Add tests

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -3,12 +3,19 @@ import { test } from "tap";
 
 const server = buildServer()
 
-test('requests the "/endpoints" route', async t => {
+test('requests the "/endpoints" route', async (t) => {
   const server = buildServer()
+
+  t.teardown(() => {
+    server.close();
+  });
+
 
   const response = await server.inject({
     method: 'GET',
     url: '/endpoints'
   })
+
   t.equal(response.statusCode, 200, 'returns a status code of 200')
+  //t.same(response.json(), {}, 'returns the endpoints')
 })
